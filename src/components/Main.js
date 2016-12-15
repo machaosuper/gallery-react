768cb873b76ec80a3e8c91c6d2dd931ceae15974
WINDOWS098\Admin <272110541@qq.com>
2016-12-15 19:06:18 +0800

    '脚手架'

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,59 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('styles/mian.scss');
+
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
+// 获取图片相关的数据
+let imageDatas = require('../data/imageDatas.json')
 
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <span>Hello world </span>
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!!!</div>
-      </div>
-    );
-  }
-}
+//利用自执行函数，将图片名信息转换成URl信息
+imageDatas = (function genImageUrl(imageDataArr) {
+	for (var i = 0; i < imageDataArr.length; i++) {
+		var singleImageData = imageDataArr[i];
+		singleImageData.imageUrl = require('../images/' + singleImageData.fileName);
+		imageDataArr[i] = singleImageData.imageUrl;
+	}
+	return imageDataArr;
+})(imageDatas)
 
-AppComponent.defaultProps = {
-};
 
-export default AppComponent;
-// module.exports = AppComponent;
+var GalleryByReactApp = React.createClass({
+	render: function (){
+		return (
+			<section className="stage">
+				<section className="img-sec">
+
+				</section>
+				<nav className="constructor-nav">
+					
+				</nav>
+			</section>
+		);
+	}
+
+
+})
+
+
+
+module.exports = GalleryByReactApp;
+
+// class AppComponent extends React.Component {
+//   render() {
+//     return (
+//       <div className="index">
+//         <img src={yeomanImage} alt="Yeoman Generator" />
+//         <span>Hello world </span>
+//         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!!!</div>
+//       </div>
+//     );
+//   }
+// }
+
+// AppComponent.defaultProps = {
+// };
+
+// export default AppComponent;
